8e1e5d6e71506a03b2ba6cdb4126ee3a7b3a8a92
Cristopher <cristopher.emodesto@gmail.com>
2022-03-12 14:53:00 -0600

    Added config firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  name: 'default',
+  firebase: {
+    config: {
+      apiKey: "AIzaSyDErESfPvyaf-yNA5mFAZXmTx6BvFHWKxU",
+      authDomain: "ecommerce-c7019.firebaseapp.com",
+      projectId: "ecommerce-c7019",
+      storageBucket: "ecommerce-c7019.appspot.com",
+      messagingSenderId: "1041643793086",
+      appId: "1:1041643793086:web:49a76fd17af70839600cef"
+    }
+  }
 };
 
 /*
